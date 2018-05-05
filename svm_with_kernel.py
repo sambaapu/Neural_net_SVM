b8802ba5def9bc8cc527d59a8990a171d85a38e3
emmey_teja <emmeyteja@gmail.com>
2018-05-05 07:39:27 +0530

    Update svm_with_kernel.py

--- svm_with_kernel.py
+++ svm_with_kernel.py
@@ -1,36 +1,39 @@
+
 '''
 # ----------------------------------------------------------------------------------
-# Project : Implementation of linear, polynomial, gaussian kernels for SVM and soft SVM
+# Project : Implementation of hard SVM and soft SVM, polynomial, gaussian kernels for SVM
 # Course  : EE 769 Introduction to Machine Learning.
-# Sources : 
-# 1. for optimization, library "CVXOPT" from cvxopt.org and concepts of optimazition 
+# Sources :
+# 1. for optimization, library "CVXOPT" from cvxopt.org and concepts of optimazition
 #    from "convex optimization by Boyd" & lecture notes.
 # 2. for understanding SVM and kernels, lecture notes.
 # 3. youtube videos to generate random data sets.
 # -----------------------------------------------------------------------------------
 '''
-print(__doc__)
+#print(__doc__)
 import numpy as np
 from numpy import linalg
+
+# for quadratic programming
 import cvxopt
 import cvxopt.solvers
 
 
 
 # The linear kernel in SVM is implemented as dot product of the features x_i, x_j
-# That is done using numpy's dot product library functions			 
+# That is done using numpy's dot product library functions
 def linear_kernel(x1, x2):
 	return np.dot(x1, x2)
 
 
 # For degree-d polynomials, the polynomial kernel is defined as[2]
-#     K( x_i, x_j ) = (<x_i,x_j>  + c )^d 
-# where x_i and x_j are vectors in the input space, i.e. vectors of features computed from training or test samples 
-# and c >= 0 is a free parameter trading off the influence of higher-order versus lower-order terms in the polynomial. 
+#     K( x_i, x_j ) = (<x_i,x_j>  + c )^d
+# where x_i and x_j are vectors in the input space, i.e. vectors of features computed from training or test samples
+# and c >= 0 is a free parameter trading off the influence of higher-order versus lower-order terms in the polynomial.
 def polynomial_kernel(x, y, p=3):
 	return (1 + np.dot(x, y)) ** p
 
-
+# gaussian kernel K( x_i, x_j ) = exp{(-1/sigma^2)(||x_i - x_j||^2)}
 
 def gaussian_kernel(x, y, sigma=5.0):
 	return np.exp(-linalg.norm(x-y)**2 / (2 * (sigma ** 2)))
@@ -42,19 +45,23 @@ class classifier_SVM(object):
 		self.C = C
 		if self.C is not None: self.C = float(self.C)
 
+	# for training
 	def fit(self, X, y):
 		n_samples, n_features = X.shape
 
 		# Gram matrix
-		# In linear algebra, the Gram matrix (Gramian matrix or Gramian) 
+		# In linear algebra, the Gram matrix (Gramian matrix or Gramian)
 		# of a set of vectors x_1, ... , x_n in an inner product space is the Hermitian matrix of inner products,
-		# whose entries are given by G_ij = < x_i , x_j > 
+		# whose entries are given by G_ij = < x_i , x_j >
 
 		K = np.zeros((n_samples, n_samples))
 		for i in range(n_samples):
 			for j in range(n_samples):
 				K[i,j] = self.kernel(X[i], X[j])
 
+		#Solves the pair of primal and dual convex quadratic programs
+		# min J = (1/2) x^TPx + q^T x \\ subject to Gx <= h , Ax = b
+		# obj function is dual of general SVM obj fun with kernels
 		P = cvxopt.matrix(np.outer(y,y) * K)
 		q = cvxopt.matrix(np.ones(n_samples) * -1)
 		A = cvxopt.matrix(y, (1,n_samples))
@@ -74,10 +81,11 @@ class classifier_SVM(object):
 		# solve QP problem
 		solution = cvxopt.solvers.qp(P, q, G, h, A, b)
 
-		# Lagrange multipliers
+		# Lagrange multipliers 'a'(alphas)
 		a = np.ravel(solution['x'])
 
 		# Support vectors have non zero lagrange multipliers
+		# alphas will always be positive acc to obj fun
 		sv = a > 1e-5
 		ind = np.arange(len(a))[sv]
 		self.a = a[sv]
@@ -85,7 +93,8 @@ class classifier_SVM(object):
 		self.sv_y = y[sv]
 		print("%d support vectors out of %d points" % (len(self.a), n_samples))
 
-		# Intercept
+		# Intercept or bias term 'b'
+		# for intercept or bias update check "understanding ML theory" by shai ben david
 		self.b = 0
 		for n in range(len(self.a)):
 			self.b += self.sv_y[n]
@@ -93,14 +102,19 @@ class classifier_SVM(object):
 		self.b /= len(self.a)
 
 		# Weight vector
+		# for weight update check "understanding ML theory" by shai ben david
 		if self.kernel == linear_kernel:
 			self.w = np.zeros(n_features)
 			for n in range(len(self.a)):
 				self.w += self.a[n] * self.sv_y[n] * self.sv[n]
 		else:
+			# update of weights depends on kernel functions
+			# hence weights updated in project()
 			self.w = None
 
 	def project(self, X):
+		# y_pred = signum(w'X+b)
+		# project() will project on to the space of 'weights'
 		if self.w is not None:
 			return np.dot(X, self.w) + self.b
 		else:
@@ -113,12 +127,14 @@ class classifier_SVM(object):
 			return y_predict + self.b
 
 	def predict(self, X):
+		# y_pred = signum(w'X+b)
+		# project() will project on to the space of 'weights'
 		return np.sign(self.project(X))
 
 if __name__ == "__main__":
 	import pylab as pl
 
-	def gen_lin_separable_data():
+	def lin_separable_data_gen():
 		# generate training data in the 2-d case
 		mean1 = np.array([0, 2])
 		mean2 = np.array([2, 0])
@@ -129,7 +145,7 @@ if __name__ == "__main__":
 		y2 = np.ones(len(X2)) * -1
 		return X1, y1, X2, y2
 
-	def gen_non_lin_separable_data():
+	def nonlin_separable_data_gen():
 		mean1 = [-1, 2]
 		mean2 = [1, -1]
 		mean3 = [4, -4]
@@ -140,21 +156,21 @@ if __name__ == "__main__":
 		y1 = np.ones(len(X1))* -1
 		X2 = np.random.multivariate_normal(mean2, cov, 50)
 		X2 = np.vstack((X2, np.random.multivariate_normal(mean4, cov, 50)))
-		y2 = np.ones(len(X2)) 
+		y2 = np.ones(len(X2))
 		return X1, y1, X2, y2
 
-	def gen_lin_separable_overlap_data():
+	def overlapdata_gen():
 		# generate training data in the 2-d case
 		mean1 = np.array([0, 2])
 		mean2 = np.array([2, 0])
-		cov = np.array([[1.5, 1.0], [1.0, 1.5]])
+		cov = np.array([[2.5, 1.0], [1.0, 2.5]])
 		X1 = np.random.multivariate_normal(mean1, cov, 100)
 		y1 = np.ones(len(X1))
 		X2 = np.random.multivariate_normal(mean2, cov, 100)
 		y2 = np.ones(len(X2)) * -1
 		return X1, y1, X2, y2
 
-	def split_train(X1, y1, X2, y2):
+	def training_data_split(X1, y1, X2, y2):
 		X1_train = X1[:90]
 		y1_train = y1[:90]
 		X2_train = X2[:90]
@@ -163,7 +179,7 @@ if __name__ == "__main__":
 		y_train = np.hstack((y1_train, y2_train))
 		return X_train, y_train
 
-	def split_test(X1, y1, X2, y2):
+	def test_data_split(X1, y1, X2, y2):
 		X1_test = X1[90:]
 		y1_test = y1[90:]
 		X2_test = X2[90:]
@@ -180,7 +196,7 @@ if __name__ == "__main__":
 
 		pl.plot(X1_train[:,0], X1_train[:,1], "ro")
 		pl.plot(X2_train[:,0], X2_train[:,1], "bo")
-		pl.scatter(clf.sv[:,0], clf.sv[:,1], s=100, c="g")
+		#pl.scatter(clf.sv[:,0], clf.sv[:,1], s=100, c="g")
 
 		# w.x + b = 0
 		a0 = -4; a1 = f(a0, clf.w, clf.b)
@@ -206,7 +222,7 @@ if __name__ == "__main__":
 	def plot_contour(X1_train, X2_train, clf , title_):
 		pl.plot(X1_train[:,0], X1_train[:,1], "ro")
 		pl.plot(X2_train[:,0], X2_train[:,1], "bo")
-		pl.scatter(clf.sv[:,0], clf.sv[:,1], s = 100, c = "g")
+		#pl.scatter(clf.sv[:,0], clf.sv[:,1], s = 100, c = "g")
 
 		X1, X2 = np.meshgrid(np.linspace(-6,6,50), np.linspace(-6,6,50))
 		X = np.array([[x1, x2] for x1, x2 in zip(np.ravel(X1), np.ravel(X2))])
@@ -214,7 +230,7 @@ if __name__ == "__main__":
 		pl.contour(X1, X2, Z, [0.0], colors ='k', linewidths = 1, origin = 'lower')
 		pl.contour(X1, X2, Z + 1, [0.0], colors='grey' , linewidths=1 , origin ='lower')
 		pl.contour(X1, X2, Z - 1, [0.0], colors='grey' , linewidths=1 , origin ='lower')
-		
+
 		pl.xlabel('X1')
 		pl.ylabel('X2')
 		pl.title(title_)
@@ -224,9 +240,9 @@ if __name__ == "__main__":
 	#------------------------------for linear Kernel-----------------------------
 
 	def test_linear():
-		X1, y1, X2, y2 = gen_lin_separable_data()
-		X_train, y_train = split_train(X1, y1, X2, y2)
-		X_test, y_test = split_test(X1, y1, X2, y2)
+		X1, y1, X2, y2 = lin_separable_data_gen()
+		X_train, y_train = training_data_split(X1, y1, X2, y2)
+		X_test, y_test = test_data_split(X1, y1, X2, y2)
 
 		clf = classifier_SVM()
 		clf.fit(X_train, y_train)
@@ -236,14 +252,15 @@ if __name__ == "__main__":
 		#acc = correct/len(y_predict)*100
 		print("%d out of %d predictions correct" % (correct, len(y_predict) ))
 
-		plot_margin(X_train[y_train == 1], X_train[y_train == -1], clf , title_ = 'linear kernel')
+		plot_margin(X_train[y_train == 1], X_train[y_train == -1], clf , title_ = 'Hard SVM')
+		#plot_margin(X_test[y_test == 1], X_test[y_test == -1], clf , title_ = 'Hard SVM')
 
 	#---------------------------------for polynomial kernel---------------------
 
 	def test_non_linear():
-		X1, y1, X2, y2 = gen_non_lin_separable_data()
-		X_train, y_train = split_train(X1, y1, X2, y2)
-		X_test, y_test = split_test(X1, y1, X2, y2)
+		X1, y1, X2, y2 = nonlin_separable_data_gen()
+		X_train, y_train = training_data_split(X1, y1, X2, y2)
+		X_test, y_test = test_data_split(X1, y1, X2, y2)
 
 		clf = classifier_SVM(polynomial_kernel)
 		clf.fit(X_train, y_train)
@@ -252,15 +269,15 @@ if __name__ == "__main__":
 		correct = np.sum(y_predict == y_test)
 		#acc = correct/len(y_predict)*100
 		print("%d out of %d predictions correct" % (correct, len(y_predict)))
-		title_ = 'polynomial kernel'
+		title_ = 'polynomial kernel with d = 3'
 		plot_contour(X_train[y_train == 1], X_train[y_train == -1], clf ,title_)
 
 
 	# ----------------------for soft SVM---------------------------------------
 	def test_soft():
-		X1, y1, X2, y2 = gen_lin_separable_overlap_data()
-		X_train, y_train = split_train(X1, y1, X2, y2)
-		X_test, y_test = split_test(X1, y1, X2, y2)
+		X1, y1, X2, y2 = overlapdata_gen()
+		X_train, y_train = training_data_split(X1, y1, X2, y2)
+		X_test, y_test = test_data_split(X1, y1, X2, y2)
 
 		clf = classifier_SVM(C = 1000.1)
 		clf.fit(X_train, y_train)
@@ -270,16 +287,16 @@ if __name__ == "__main__":
 		#acc = correct/len(y_predict)*100
 		print("%f out of %f predictions correct" % (correct, len(y_predict)))
 
-		title_ = 'Soft SVM kernel'
+		title_ = 'Soft SVM'
 		plot_contour(X_train[y_train == 1], X_train[y_train == -1], clf , title_)
-	
+
 
 
 	# ---------------for gaussian kernels--------------------------------------
 	def test_gaussian_nonlinear():
-		X1, y1, X2, y2 = gen_non_lin_separable_data()
-		X_train, y_train = split_train(X1, y1, X2, y2)
-		X_test, y_test = split_test(X1, y1, X2, y2)
+		X1, y1, X2, y2 = nonlin_separable_data_gen()
+		X_train, y_train = training_data_split(X1, y1, X2, y2)
+		X_test, y_test = test_data_split(X1, y1, X2, y2)
 
 		clf = classifier_SVM(gaussian_kernel)
 		clf.fit(X_train, y_train)
@@ -288,25 +305,28 @@ if __name__ == "__main__":
 		correct = np.sum(y_predict == y_test)
 		#acc = correct/len(y_predict)*100
 		print("%d out of %d predictions correct" %(correct, len(y_predict)))
-		
 
-		title_ = 'RBF kernel'
+
+		title_ = 'RBF kernel with sigma = 5.0'
 		plot_contour(X_train[y_train == 1], X_train[y_train == -1], clf , title_)
 
 
-	print("\nSoft SVM performing on the Linear separable Gaussian overlapped data generated\n")
+	# for Soft SvM testing uncomment the following
+	#print("\nSoft SVM performing on the Linear separable Gaussian overlapped data generated\n")
 	#test_soft()
 
-	
-	print("\nLinear kernel performing on the Linear separable Gaussian data generated\n")
-	test_linear()
+	#for Hard SVM test uncomment the following
+	#print("\nLinear kernel performing on the Linear separable Gaussian data generated\n")
+	#test_linear()
 
-	print("\nNonLinear Polynomial kernel of order : 3\tperforming on the Gaussian data generated\n")
-	test_non_linear()
+	# for testing of polynomial kernel SVM uncomment the following
+	#print("\nNonLinear Polynomial kernel of order : 3 performing on the Gaussian data generated\n")
+	#test_non_linear()
 
+	# for testing of gaussian kernel SVM uncomment the following
 	print("\nGaussian Kernel\n")
 	test_gaussian_nonlinear()
-	
+
 	#--------------------------------------------------------------------------------------
 	#----------------------------THE END---------------------------------------------------
 	#--------------------------------------------------------------------------------------
